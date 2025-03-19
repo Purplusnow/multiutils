6a21bc59de55f0217ecbcd850f973748d65dbad8
Kyungbin Cho <34591718+Purplusnow@users.noreply.github.com>
2025-03-19 22:47:06 +0900

    1

--- tools/calculator/script.js
+++ tools/calculator/script.js
@@ -1,5 +1,19 @@
 let display = document.getElementById("display");
 
+document.addEventListener("keydown", function(event) {
+    if (event.key >= '0' && event.key <= '9') {
+        appendNumber(event.key);
+    } else if (event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/") {
+        appendOperator(event.key);
+    } else if (event.key === "Enter") {
+        calculateResult();
+    } else if (event.key === "Backspace") {
+        deleteLast();
+    } else if (event.key === ".") {
+        appendOperator(".");
+    }
+});
+
 function appendNumber(number) {
     display.value += number;
 }
@@ -8,12 +22,12 @@ function appendOperator(operator) {
     display.value += operator;
 }
 
-function appendFunction(func) {
-    display.value += func;
-}
-
 function clearDisplay() {
     display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
 }
 
 function calculateResult() {
